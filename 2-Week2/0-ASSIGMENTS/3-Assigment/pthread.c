8edaca60227ae5bf750a00339ada7534c57ad50f
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2022-01-13 22:00:09 +0100

    Some improvements to the assignmentwq

--- 2-Week2/0-ASSIGMENTS/3-Assigment/pthread.c
+++ 2-Week2/0-ASSIGMENTS/3-Assigment/pthread.c
@@ -7,7 +7,6 @@
 #include <sys/resource.h>
 #include <sys/types.h>
 #include <syslog.h>
-#define COUNT  1000
 #define N_THREADS 128
 
 typedef struct
@@ -15,7 +14,6 @@ typedef struct
     int threadIdx;
 } threadParams_t;
 
-struct sched_param fifo_sched;
 
 
 void print_scheduler(void);
@@ -23,6 +21,8 @@ void print_scheduler(void);
 //Starter Thread, attribute and function
 pthread_t starter_thread;
 pthread_attr_t starter_attr;
+struct sched_param fifo_sched;
+
 void * starter_func(void * a);
 
 //working Threads, param and attributes
@@ -36,13 +36,12 @@ void * working_func(void * working_attr);
 // Set the attributes to FIFO
 void set_scheduler(void){
 
-    // Primero attributos de starter
-    //Init
+    // First set starter thread policy to FIFO and affinity. It will run on the core 1
     pthread_attr_init(&starter_attr);
     pthread_attr_setschedpolicy(&starter_attr,SCHED_FIFO); //Set to RT extension policy
     fifo_sched.sched_priority=sched_get_priority_max(SCHED_FIFO);
 
-    sched_setscheduler(getpid(), SCHED_FIFO, &fifo_sched); //give SCHED_FIFO a main tambien
+    sched_setscheduler(getpid(), SCHED_FIFO, &fifo_sched); //give SCHED_FIFO a main
 
     pthread_attr_setschedparam(&starter_attr,&fifo_sched);
     cpu_set_t cpu;
@@ -50,6 +49,7 @@ void set_scheduler(void){
     CPU_SET(1,&cpu);
     pthread_attr_setaffinity_np(&starter_attr,sizeof(cpu_set_t),&cpu);
 
+    // Now set working threads policy to FIFO and priorities so the less index max priority. So logs will be ordererd
     for (int i =0;i < N_THREADS;i++){
 
         workers_param[i].threadIdx=i;
@@ -61,32 +61,25 @@ void set_scheduler(void){
         CPU_ZERO(&cpu_thread);
         CPU_SET(3,&cpu_thread);
         pthread_attr_setaffinity_np(&workers_attr[i],sizeof(cpu_set_t),&cpu_thread);
-
-        pthread_create(&workers_thread[i],
-                        &workers_attr[i],
-                        working_func,
-                        (void *)&workers_param[i]);
-
     }
 
     
 }
 
 
-
-
-
 int main(){
 
+
+    // Invoke the function to set the policy
     set_scheduler();
     
-
+    // Create the starter FIFO thread that will launch others
     pthread_create(&starter_thread,
                     &starter_attr,
                     starter_func,
                     (void *)0);
 
-    
+    // Join the starter thread
     pthread_join(starter_thread,NULL);
 
 }
@@ -97,13 +90,21 @@ void * starter_func(void * arg){
     int pid = getpid();
     int rc=sched_getparam(pid, &res);
     int b = res.sched_priority;
-    int algo = sched_get_priority_max(SCHED_FIFO);
-    //int a = getpriority(gettid(),0);
-    //printf("Hola desde Starter con prioridad %d \n",b);
-    //printf("MAxima prioridad es = %d\n",algo);
+
+    for (unsigned int i = 0;i<N_THREADS;i++){
+
+        pthread_create(&workers_thread[i],
+                        &workers_attr[i],
+                        working_func,
+                        (void *)&workers_param[i]);
+    }
 
 
-   // print_scheduler();
+
+
+    for(unsigned int i = 0;i<N_THREADS;i++){
+        pthread_join(workers_thread[i],NULL);
+    }
 }
 
 void * working_func(void * working_attr){
